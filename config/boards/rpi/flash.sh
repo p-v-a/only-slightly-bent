1db3a2acf5d7a7648f3e046f8593abe5c252b208
Vladimir Pasashnikov <vladimir.pasashnikov@gmail.com>
2024-04-15 21:53:09 +1000

    only run certain commands under root, rather then whole flash.sh script

--- config/boards/rpi/flash.sh
+++ config/boards/rpi/flash.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]
-  then echo "ERROR: Please run as root"
-  exit
-fi
-
 pushd /home/klipper/klipper
-systemctl stop klipper.service
-systemctl stop klipper-mcu.service
+sudo systemctl stop klipper.service
+sudo systemctl stop klipper-mcu.service
+
 make flash
-# Reset ownership
-chown klipper:klipper -R /home/klipper/klipper
 
-cp ./scripts/klipper-mcu.service /etc/systemd/system/
-systemctl enable klipper-mcu.service
+sudo cp ./scripts/klipper-mcu.service /etc/systemd/system/
+sudo chown root:root /etc/systemd/system/klipper-mcu.service
+sudo systemctl enable klipper-mcu.service
 
-systemctl start klipper-mcu.service
-systemctl start klipper.service
+sudo systemctl start klipper-mcu.service
+sudo systemctl start klipper.service
 popd
