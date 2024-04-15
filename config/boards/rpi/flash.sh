f31f4eae4b695916f3f64cf584ec26c795515b80
Vladimir Pasashnikov <vladimir.pasashnikov@gmail.com>
2024-04-15 11:53:42 +1000

    ensure rpi flash.sh enables klipper-mcu service

--- config/boards/rpi/flash.sh
+++ config/boards/rpi/flash.sh
@@ -6,5 +6,8 @@ make flash
 # Reset ownership
 chown klipper:klipper -R /home/klipper/klipper
 
+cp ./scripts/klipper-mcu.service /etc/systemd/system/
+systemctl enable klipper-mcu.service
+
 service klipper start
 popd
